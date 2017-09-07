a0166fae3792458c0b9a5a90b8ecd98ff7737b98
vannucci@gmail.com <vannucci@gmail.com>
2017-09-07 19:28:25 -0400

    Adding routes which work! Hoorah!

--- controllers/maincontrollers.js
+++ controllers/maincontrollers.js
@@ -1,9 +1,33 @@
 var mongoose = require('mongoose');
 var Posts = require('../models/posts.js');
-var axios = require('axios');
+var Comments = require('../models/posts.js');
+mongoose.Promise = global.Promise;
 
 
 
 module.exports = function(app) {
+
+	app.get('/custom', function(req,res) {
+		res.send("Got it");
+
+	});
+
+	app.post('/newpost', function(req,res) {
+		var newPost = new Posts({
+			author: req.author,
+			title: req.title,
+			body: req.body,
+			date: new Date()
+		});
+
+		newPost.save(function(err) {
+			if(err) throw err;
+			res.send("Saved!");
+		});
+	});
+
+	app.post('/newcomment', function(req,res) {
+
+	})
 
 };
