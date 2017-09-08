d372dfb6785e664e62eb6f24f1816e9e57bde136
vannucci@gmail.com <vannucci@gmail.com>
2017-09-07 21:01:01 -0400

    Changed route names, got rid of test route

--- controllers/maincontrollers.js
+++ controllers/maincontrollers.js
@@ -9,12 +9,7 @@ mongoose.Promise = global.Promise;
 
 module.exports = function(app) {
 
-	app.get('/custom', function(req,res) {
-		res.send("Got it");
-
-	});
-
-	app.post('/newpost', function(req,res) {
+	app.post('/api/newpost', function(req,res) {
 		console.log(req.body);
 		var newPost = new Posts(req.body);
 
@@ -24,7 +19,7 @@ module.exports = function(app) {
 		});
 	});
 
-	app.post('/newcomment', function(req,res) {
+	app.post('/api/newcomment', function(req,res) {
 		var newComment = new Comments(req.body);
 
 		newComment.save(function(err) {
@@ -34,7 +29,7 @@ module.exports = function(app) {
 
 	});
 
-	app.get('/getposts', function(req,res) {
+	app.get('/api/getposts', function(req,res) {
 		Posts.find()
 			.then(function(data) {
 				console.log(data);
