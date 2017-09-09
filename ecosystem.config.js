926ea728b629db64d8340cd8c35049dc86ccae0c
bryan.modula <bryan.modula@gmail.com>
2017-09-09 11:16:58 -0400

    changed IP address to  elastic so that it wouldnt change on us

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-221-27-148.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-220-208-55.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/pemkeys/codersHaven.pem',
       ref: 'origin/master',
       repo: 'https://github.com/brm5050/CodersHaven.git',
