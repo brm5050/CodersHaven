5df2443d17eed49927e5d53a553f664fd3ce3a5a
paradigm1984 <danblanco1990@gmail.com>
2017-09-13 11:03:27 -0400

    got the http request to the back end. mongodb is not saving

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import React from "react";
 import {Router, Route, hashHistory, IndexRoute} from "react-router";
 
 // components / styles
-import SignUp from "./views/signUp";
 import Posts from "./views/posts";
 import CreateArticle from "./views/createArticle";
 import App from "./components/App";
@@ -11,7 +10,6 @@ import App from "./components/App";
 const Routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={Posts}/>
-		<Route path="/signup" component={SignUp}/>
 		<Route path="/posts/create" component={CreateArticle}/>
 	</Route>
 
